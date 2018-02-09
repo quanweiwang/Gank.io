562106a805acc2fd11df14d1fb23fe09762d2aa9
王权伟 <629526514@qq.com>
2018-02-09 17:57:43 +0800

    11
    11

---
+++ Gank.io/Today/Model/GKTodayModel.h
@@ -0,0 +1,23 @@
+//
+//  GKTodayModel.h
+//  Gank.io
+//
+//  Created by 王权伟 on 2018/2/9.
+//  Copyright © 2018年 王权伟. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GKTodayModel : NSObject
+
+@property(strong, nonatomic) NSString * createdAt;
+@property(strong, nonatomic) NSString * desc;
+@property(strong, nonatomic) NSString * images;
+@property(strong, nonatomic) NSString * publishedAt;
+@property(strong, nonatomic) NSString * source;
+@property(strong, nonatomic) NSString * type;
+@property(strong, nonatomic) NSString * url;
+@property(strong, nonatomic) NSString * used;
+@property(strong, nonatomic) NSString * who;
+
+@end
---
+++ Gank.io/Today/Model/GKTodayModel.m
@@ -0,0 +1,15 @@
+//
+//  GKTodayModel.m
+//  Gank.io
+//
+//  Created by 王权伟 on 2018/2/9.
+//  Copyright © 2018年 王权伟. All rights reserved.
+//
+
+#import "GKTodayModel.h"
+
+@implementation GKTodayModel
+
+
+
+@end
